a962d47684b80fb11e82a347c542b2fe82e47777
Yuta Ohira <alesion90@gmail.com>
2023-06-07 07:14:15 +0900

    zValidator

--- src/api/example/index.ts
+++ src/api/example/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const exampleRoute = new Hono();
 
@@ -10,23 +11,27 @@ const doraemonQuestionQuerySchema = z.object({
   question: z.string().min(1),
 });
 
-exampleRoute.get("/doraemon_question", async (c) => {
-  const query = doraemonQuestionQuerySchema.safeParse(c.req.query());
-  if (query.success === false) {
-    console.error(query.error);
-    throw new HTTPException(400, { message: "Bad Request" });
+exampleRoute.get(
+  "/doraemon_question",
+  zValidator("query", doraemonQuestionQuerySchema, (result) => {
+    if (!result.success) {
+      const errorMessage = result.error.issues.map((i) => i.message).join("\n");
+      throw new HTTPException(400, { message: errorMessage });
+    }
+  }),
+  async (c) => {
+    const query = c.req.valid("query");
+    const res = await chat.call([
+      new SystemChatMessage(
+        "あなたはドラえもんです。ユーザーの困りごとに対して, ひみつ道具で解決してください。"
+      ),
+      new HumanChatMessage(query.question),
+    ]);
+
+    return c.json({
+      message: res.text,
+    });
   }
-
-  const res = await chat.call([
-    new SystemChatMessage(
-      "あなたはドラえもんです。ユーザーの困りごとに対して, ひみつ道具で解決してください。"
-    ),
-    new HumanChatMessage(query.data.question),
-  ]);
-
-  return c.json({
-    message: res.text,
-  });
-});
+);
 
 export default exampleRoute;
