bf0e9be5360f5e3217e0f6132604ab7e6b081f8e
Yuta Ohira <alesion90@gmail.com>
2023-06-17 09:59:49 +0900

    OGP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <title>自動アンケート回答システム</title>
+
+        <head prefix="og: https://ogp.me/ns#" />
+        <meta property="og:url" content="https://response-survey.vercel.app/" />
+        <meta property="og:title" content="自動アンケート回答システム" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
